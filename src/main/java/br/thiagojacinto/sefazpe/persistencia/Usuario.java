c7724269c860da8f268de84db92bdb07458fdcb0
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-20 11:13:55 -0300

    [add] serializable + timestamp de criação & atualização pelo Hibernate

--- src/main/java/br/thiagojacinto/sefazpe/persistencia/Usuario.java
+++ src/main/java/br/thiagojacinto/sefazpe/persistencia/Usuario.java
@@ -1,5 +1,7 @@
 package br.thiagojacinto.sefazpe.persistencia;
 
+import java.io.Serializable;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,16 +10,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import br.thiagojacinto.sefazpe.negocio.Utilidades;
 
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+@NamedQueries({
+	@NamedQuery(name = "Usuario.mostrarTodos", query = "SELECT usuario FROM Usuario usuario")
+})
+public class Usuario implements Serializable {
+
+	/**
+	 * Implementando `serializable`:
+	 * 
+	 * 	Prática de mercado
+	 */
+	private static final long serialVersionUID = 1L;
 
 	// Atributos
-	
+		
 	// JPA -> É necessário um atributo para o índice
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,6 +52,12 @@ public class Usuario {
 	
 	@Column(name = "telefones")
 	private String telefonesString;
+	
+	@CreationTimestamp
+	private Date criadoEm;
+	
+	@UpdateTimestamp
+	private Date atualizadoEm;
 
 	// Construtores:
 
