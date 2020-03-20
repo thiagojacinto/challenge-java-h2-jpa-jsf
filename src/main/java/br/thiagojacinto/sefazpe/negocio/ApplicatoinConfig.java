fa9afb1a4af86591ab21f2fd4a84d5102445248f
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-20 13:45:28 -0300

    [novo] configuração da URI principal

---
+++ src/main/java/br/thiagojacinto/sefazpe/negocio/ApplicatoinConfig.java
@@ -0,0 +1,9 @@
+package br.thiagojacinto.sefazpe.negocio;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class ApplicatoinConfig extends Application {
+	// Define a base para os futuros `endpoints`
+}
