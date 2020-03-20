af47aac7971f1d3ce5450783d32b4e30bd537dea
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-20 13:46:04 -0300

    [fix] sysout/placeholder removido

--- src/main/java/br/thiagojacinto/sefazpe/App.java
+++ src/main/java/br/thiagojacinto/sefazpe/App.java
@@ -1,13 +1,14 @@
 package br.thiagojacinto.sefazpe;
 
 /**
- * Hello world!
- *
+ * 		Arquivo MAIN
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+	
+    public static void main( String[] args ) {
+        
+    	
+    	
     }
 }
