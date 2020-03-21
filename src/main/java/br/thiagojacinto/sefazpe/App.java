d80846aed8215a07b2861f5e189b956e81250818
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-03-21 14:15:46 -0300

    [todo] operação no Main

--- src/main/java/br/thiagojacinto/sefazpe/App.java
+++ src/main/java/br/thiagojacinto/sefazpe/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	/*
+    	 * TODO: Ajustar os métodos da API no Main.
+    	 */
+    	System.out.println( "Hello World!" );
     }
 }
